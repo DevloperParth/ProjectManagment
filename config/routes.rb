6a3b9f52d367dff3451f5e8c2bf81d4fb8a62f24
aman-vyas-123 <amvyas@bestpeers.com>
2023-06-07 16:54:01 +0530

    REC-1 Creating Employers Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  root "employers#index"
+
+  resources :employers
+  devise_for :employers
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
