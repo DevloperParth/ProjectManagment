d1f4b6804c7016d3caf4620ce383ef494e59d25d
aman-vyas-123 <amvyas@bestpeers.com>
2023-06-08 12:20:35 +0530

    REC-3 Creating Jobs details  Views

--- app/controllers/job_details_controller.rb
+++ app/controllers/job_details_controller.rb
@@ -20,11 +20,33 @@ class JobDetailsController < ApplicationController
     end
   end
 
+  def edit
+    @job_detail = JobDetail.find(params[:id])
+  end
+
+  def update
+    @job_detail = JobDetail.find(params[:id])
+
+    if @job_detail.update(job_params)
+      redirect_to @job_detail
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @job_detail = JobDetail.find(params[:id])
+    @job_detail.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def job_params
       params.require(:job_detail).permit(:employer_id, :Job_title, :Job_summary,
       :Qualification_skills, :Experience, :salary)
     end
+
 
 end
 
